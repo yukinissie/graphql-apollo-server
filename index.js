5f58d9542741d58d1a7fb3097083acbc176919de
yukinissie <bigtree2490@outlook.jp>
2022-08-05 23:20:34 +0900

    Add axios and myPosts

--- index.js
+++ index.js
@@ -1,29 +1,54 @@
 const { ApolloServer, gql } = require("apollo-server");
-
-const users = [
-  { id: "1", name: "John Doe", email: "john@test.com" },
-  { id: "2", name: "Jane Doe", email: "jane@example.com" },
-];
+const { default: axios } = require("axios");
+const { assertWrappingType } = require("graphql");
 
 const typeDefs = gql`
   type User {
     id: ID!
     name: String!
     email: String!
+    myPosts: [Post]
+  }
+
+  type Post {
+    id: ID!
+    title: String!
+    body: String!
+    userId: ID!
   }
 
   type Query {
     hello(name: String!): String
     users: [User]
     user(id: ID!): User
+    posts: [Post]
   }
 `;
 
 const resolvers = {
   Query: {
     hello: (parent, args) => `Hello, ${args.name}!`,
-    users: () => users,
-    user: (parent, args) => users.find((user) => user.id === args.id),
+    users: async () => {
+      const res = await axios.get("https://jsonplaceholder.typicode.com/users");
+      return res.data;
+    },
+    user: async (parent, args) => {
+      const res = await axios.get(
+        `https://jsonplaceholder.typicode.com/users/${args.id}`
+      );
+      return res.data;
+    },
+    posts: async () => {
+      const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+      return res.data;
+    },
+  },
+  User: {
+    myPosts: async (parent) => {
+      const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+      const myPosts = res.data.filter((post) => post.userId == parent.id);
+      return myPosts;
+    },
   },
 };
 
